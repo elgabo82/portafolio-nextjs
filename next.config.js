7d5638bde94b31f0dd211278cf3fedec683395b5
elgabo82 <gabrielmorejon@gmail.com>
2021-10-05 19:47:50 -0500

    Cambios en next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/twitter': { page: '/twitter' }//,
+      // '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      // '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      // '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+    }
+  },
   // webpack(config) {
   //   config.resolve.modules.push(__dirname)
   //   return config;
